b266f8fecc79ed43211b75dd784edad8e6030549
wellbell <uok02018496@naver.com>
2021-06-02 05:31:05 +0900

    Initial commit

---
+++ src/test/java/springbootAWSS3/SpringbootAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootAWSS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
