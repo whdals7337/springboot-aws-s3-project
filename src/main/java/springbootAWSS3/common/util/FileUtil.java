0bdb3c6428795b1e6c297c420b4f289f279f9198
wellbell <uok02018496@naver.com>
2021-06-02 09:41:42 +0900

    [refactor] 파일 다운로드시 브라우저별 파일 이름 인코딩 메서드 FileUtil 로 이동

--- src/main/java/springbootAWSS3/common/util/FileUtil.java
+++ src/main/java/springbootAWSS3/common/util/FileUtil.java
@@ -2,8 +2,13 @@ package springbootAWSS3.common.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Enumeration;
 
 public class FileUtil {
 
@@ -22,5 +27,59 @@ public class FileUtil {
         String extension = fileName.substring(fileName.lastIndexOf("."));
         return RandomStringUtils.randomAlphabetic(5)+ "_" + RandomStringUtils.randomNumeric(5) + "_" +
                 LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) + extension;
+    }
+
+    public static String getFileNameByBrowser(String fileName, HttpServletRequest request)
+            throws UnsupportedEncodingException {
+        String browser= "";
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            if(headerName.equals("user-agent")) {
+                browser = request.getHeader(headerName);
+            }
+        }
+
+        String docName = "";
+        if (browser.contains("Trident") || browser.contains("MSIE") || browser.contains("Edge")) {
+            docName = FileUtil.mappingSpecialCharacter(URLEncoder.encode(fileName, "UTF-8"));
+
+        } else if (browser.contains("Firefox")) {
+            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+
+        } else if (browser.contains("Opera")) {
+            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+
+        } else if (browser.contains("Chrome")) {
+            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+
+        } else if (browser.contains("Safari")) {
+            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+
+        } else {
+            //throw new RuntimeException("미지원 브라우저");
+        }
+        return docName;
+    }
+
+    public static String mappingSpecialCharacter(String name) {
+
+        // 파일명에 사용되는 특수문자
+        char[] sh_list = { '~', '!', '@', '#', '$', '%', '&', '(', ')', '=', ';', '[', ']', '{', '}', '^', '-' };
+        try {
+            for (char sh : sh_list) {
+                String encodeStr = URLEncoder.encode(sh + "", "UTF-8");
+                name = name.replaceAll(encodeStr, "\\" + sh);
+            }
+
+            // 띄워쓰기 -> + 치환
+            name = name.replaceAll("%2B", "+");
+            // 콤마 -> _ 치환
+            name = name.replaceAll("%2C", "_");
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return name;
     }
 }
--- src/main/java/springbootAWSS3/controller/FileInfoController.java
+++ src/main/java/springbootAWSS3/controller/FileInfoController.java
@@ -9,16 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import springbootAWSS3.common.uploader.Uploader;
+import springbootAWSS3.common.util.FileUtil;
 import springbootAWSS3.domain.Entity.FileInfo;
 import springbootAWSS3.repository.FileInfoRepository;
 import springbootAWSS3.service.FileInfoService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -45,66 +42,12 @@ public class FileInfoController {
         FileInfo fileInfo = fileInfoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("존재 하지 않는 파일"));
 
-        String filename = getFileNameByBrowser(fileInfo.getFileOriginName(), request);
+        String filename = FileUtil.getFileNameByBrowser(fileInfo.getFileOriginName(), request);
         Resource resource = uploader.downloadResource(fileInfo.S3key());
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
                 .body(resource);
-    }
-
-    protected String getFileNameByBrowser(String fileName, HttpServletRequest request)
-            throws UnsupportedEncodingException {
-        String browser= "";
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            if(headerName.equals("user-agent")) {
-                browser = request.getHeader(headerName);
-            }
-        }
-
-        String docName = "";
-        if (browser.contains("Trident") || browser.contains("MSIE") || browser.contains("Edge")) {
-            docName = mappingSpecialCharacter(URLEncoder.encode(fileName, "UTF-8"));
-
-        } else if (browser.contains("Firefox")) {
-            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-
-        } else if (browser.contains("Opera")) {
-            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-
-        } else if (browser.contains("Chrome")) {
-            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-
-        } else if (browser.contains("Safari")) {
-            docName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-
-        } else {
-            //throw new RuntimeException("미지원 브라우저");
-        }
-        return docName;
-    }
-
-    public static String mappingSpecialCharacter(String name) {
-
-        // 파일명에 사용되는 특수문자
-        char[] sh_list = { '~', '!', '@', '#', '$', '%', '&', '(', ')', '=', ';', '[', ']', '{', '}', '^', '-' };
-        try {
-            for (char sh : sh_list) {
-                String encodeStr = URLEncoder.encode(sh + "", "UTF-8");
-                name = name.replaceAll(encodeStr, "\\" + sh);
-            }
-
-            // 띄워쓰기 -> + 치환
-            name = name.replaceAll("%2B", "+");
-            // 콤마 -> _ 치환
-            name = name.replaceAll("%2C", "_");
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return name;
     }
 }
